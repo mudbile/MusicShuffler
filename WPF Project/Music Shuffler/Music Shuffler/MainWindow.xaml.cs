21d155a6f30066e2940966ed8ffd17d80737def8
Daniel <dcdowsett@gmail.com>
2017-05-28 13:05:04 +1000

    Added folder dialogue to pick rootFolder

--- WPF Project/Music Shuffler/Music Shuffler/MainWindow.xaml.cs
+++ WPF Project/Music Shuffler/Music Shuffler/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Music_Shuffler {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        Playlist playlist;
+        List<String> musicFileExtensions = new List<string>() { ".mp3", ".wav" };
         public MainWindow() {
             InitializeComponent();
-            List<String> extensions = new List<string>(){ ".mp3", ".wav" };
-            Playlist playlist = new Playlist("C://users/daniel/documents/music", extensions);
+        }
+
+
+        public void btnChooseRootClicked(object sender, RoutedEventArgs e) {
+            using (CommonOpenFileDialog dialog = new CommonOpenFileDialog()) {
+                dialog.IsFolderPicker = true;
+                if (dialog.ShowDialog() != CommonFileDialogResult.Ok) {
+                    return;
+                }
+                txtRootFolder.Text = dialog.FileName;
+            }
+        }
+
+            
+        public void btnGetAlbumsClicked(object sender, RoutedEventArgs eevent) {
+            String rootFolder = txtRootFolder.Text;
+            if (!Directory.Exists(rootFolder)) {
+                MessageBox.Show("Folder does not exist");
+                return;
+            }
+            playlist = new Playlist(rootFolder, musicFileExtensions);
         }
     }
 }
